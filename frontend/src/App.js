19433f8e37fd89ec1ab2c48f1fef9b61bac726ab
Marcos <marcos.carbajal@mercadolibre.com>
2020-04-20 15:00:25 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <strong>Solidity frontend experiment</strong>, by the amargos club.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
